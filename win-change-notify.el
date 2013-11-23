83e39b37f07e2e7979305c77b2bed711b8977216
sabof <esabof@gmail.com>
2013-11-23 20:41:58 +0000

    Better definition of "change"

--- win-change-notify.el
+++ win-change-notify.el
@@ -20,6 +20,16 @@
   (delete-all-overlays)
   (setq window-change-notify-window-alist))
 
+(defun wcn/cleanup ()
+  (setq window-change-notify-window-alist
+        (cl-remove-if-not
+         (lambda (pair)
+           (or (window-live-p (car pair))
+               (progn
+                 (delete-overlay (cdr (assq 'overlay (cdr pair))))
+                 nil)))
+         window-change-notify-window-alist)))
+
 (defun wcn/redraw-all-windows ()
   (wcn/clear-overlays)
   (cl-dolist (window (get-buffer-window-list nil nil t))
@@ -28,24 +38,34 @@
 
 ;; -----------------------------------------------------------------------------
 
-(cl-defun window-change-notify-hook ()
+(cl-defun window-change-notify-hook (&optional force)
   (let* (( redraw-func window-change-notify-function)
          ( string (with-temp-buffer
-                    (apply redraw-func
-                           (if (eq window-change-notify-format 'pixels)
-                               (list (es-window-inside-pixel-width)
-                                     (es-window-inside-pixel-height))
-                             (list (window-body-width)
-                                   (window-body-height))))
-                    ;; "abc"
-                    (buffer-string)
-                    ))
-         ( ov (make-overlay (point-min) (point-max))))
-    (overlay-put ov 'display string)
-    (overlay-put ov 'window (selected-window))
-    (wcn/acons (selected-window)
-               ov
-               window-change-notify-window-alist)
+                    (funcall redraw-func)
+                    (buffer-string)))
+         ( alist (or (cdr (assq (selected-window)
+                                window-change-notify-window-alist))
+                     (let* (( temp-overlay (make-overlay (point-min) (point-max)))
+                            ( temp-alist (list (cons 'window (selected-window))
+                                               (cons 'width (window-width))
+                                               (cons 'height (window-height))
+                                               (cons 'overlay temp-overlay)
+                                               )))
+                       (overlay-put temp-overlay 'window (selected-window))
+                       (setq window-change-notify-window-alist
+                             (cl-acons (selected-window) temp-alist
+                                       window-change-notify-window-alist))
+                       temp-alist
+                       )))
+         ( ov (cdr (assq 'overlay alist))))
+    (unless (and (not force)
+                 (= (cdr (assq 'height alist)) (window-height))
+                 (= (cdr (assq 'width alist)) (window-width)))
+      (overlay-put ov 'display string)
+      (setf (cdr (assq 'height alist)) (window-height))
+      (setf (cdr (assq 'width alist)) (window-width))
+      (overlay-put ov 'width string)
+      )
     (set-window-start nil (point-min))
     ))
 
@@ -58,7 +78,7 @@
     (erase-buffer)
     (insert "$\n"))
   (setq-local auto-window-vscroll nil)
-  (setq-local auto-window- nil)
+  (setq-local cursor-type nil)
   (add-hook 'window-configuration-change-hook
             'window-change-notify-hook nil t)
   (wcn/redraw-all-windows))
@@ -67,16 +87,18 @@
 ;; Example
 ;; -----------------------------------------------------------------------------
 
-(defun $-fill (width height)
-  (insert "\n")
-  (cl-loop repeat (- height 2) do
-           (insert " ")
-           (cl-loop repeat (- width 2) do
-                    (insert "$"))
-           (insert "\n"))
-  (add-text-properties
-   (point-min) (point-max)
-   (list 'face `(:foreground ,(es-color-random-hex)))))
+(defun $-fill ()
+  (let ((width (window-body-width))
+        (height (window-body-height)))
+    (insert "\n")
+    (cl-loop repeat (- height 2) do
+             (insert " ")
+             (cl-loop repeat (- width 2) do
+                      (insert "$"))
+             (insert "\n"))
+    (add-text-properties
+     (point-min) (point-max)
+     (list 'face `(:foreground ,(es-color-random-hex))))))
 
 (define-derived-mode $-mode window-change-notify-mode
     "$" "$"
